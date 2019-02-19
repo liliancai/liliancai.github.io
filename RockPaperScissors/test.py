d59773a967b314f0457c52cadd97c093067a1035
liliancai <yushunchn@gmail.com>
2019-02-19 18:31:23 +0100

    unit test for modal

--- RockPaperScissors/test.py
+++ RockPaperScissors/test.py
@@ -4,10 +4,16 @@ from selenium.webdriver.common.keys import Keys
 #check if homepage jump to rock paper scissors
 testLink="RockPaperScissors"
 driver=webdriver.Firefox()
-driver.get("https://github.com/liliancai/liliancai.github.io")
+driver.get("https://liliancai.github.io")
+#driver.implicitly_wait(100)
+#local test
+#driver.get("file:///home/lilian/liliancai.github.io/index.html")
 click=driver.find_element_by_partial_link_text('Rock')
 click.click()
 assert testLink in driver.page_source
 
+modal=driver.find_element_by_name("button")
+modal.click()
+assert "Ready?go!" in driver.page_source
 
 driver.close()
