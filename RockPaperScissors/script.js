83a50a437a919f0676ae77df5fe0f21bab0e2d6e
liliancai <yushunchn@gmail.com>
2019-02-23 18:27:20 +0100

    out game tested

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -7,11 +7,35 @@
 //button=getElementById('rock');
 //get button, get table, get scissor;
 
+var yourScore=0;
+var computerScore=0;
+var rounds=0;
+
 var gameStarted=0;
-//user click the ready button, then round from 0ornull to 1,score 0:0
+var gameEnded=0;
+
+// user click the ready button, then round from 0ornull to 1,score 0:0
 function startTheGame(){
   document.getElementById('modal').style.display='none';
-  gameStarted=1;
+  document.querySelector('.round').innerHTML=`Round ${rounds}: Select Rock, Paper, or Scissors.`;
+  document.querySelector('.yourScore').innerHTML=yourScore;
+  document.querySelector('.computerScore').innerHTML=computerScore;
+  gameStarted=1;  //for qUnit test if startTheGame executed or not
+}
+
+
+ function playTheGame(){
+   rounds++;
+   document.querySelector('.round').innerHTML=`Round ${rounds}: Select Rock, Paper, or Scissors.`;
+
+
+
+   //if rounds more than 10, end game
+   if(rounds >=10){
+     gameEnded=1;
+     document.querySelector('.modal-content>p').innerHTML="Game over,start again?";
+     document.getElementById('modal').style.display='block';
+   }
 }
 
 document.querySelector('#ready').addEventListener('click',()=> {
@@ -20,6 +44,12 @@ document.querySelector('#ready').addEventListener('click',()=> {
 });
 
 
+ document.querySelectorAll('.buttons').forEach(button =>{
+   button.addEventListener('click',()=>{
+        playTheGame();
+  });
+ });
+// });
 
 
-//1st one should press ready button and see round1 score 0:0
+//1st one should press ready button and see round1 score 0:
--- RockPaperScissors/selenium_user.py
+++ RockPaperScissors/selenium_user.py
@@ -9,6 +9,7 @@ class RockPaperScissorsTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     #check if homepage jump to rock paper scissors
+
     @skip
     def test_redirect_to_rock_page(self):
         self.browser.get("https://liliancai.github.io")
@@ -20,10 +21,25 @@ class RockPaperScissorsTest(unittest.TestCase):
         self.assertIn(testLink,self.browser.page_source)
         #assert testLink in self.browser.page_source
 
+    @skip
     def test_modal_start_game(self):
-        modal=self.browser.find_element_by_name("button")
+        # a question: do I have to get url each single time??
+        self.browser.get("file:///home/lilian/liliancai.github.io/RockPaperScissors/index.html")
+        modal=self.browser.find_element_by_css_selector('#ready')
+        assert "Round 1" not in self.browser.page_source
         modal.click()
-        assert "Ready?go!" not in self.browser.page_source
+        assert "Round 1" in self.browser.page_source
+
+    # click the buttons 10 times
+    def test_if_game_end(self):
+        self.browser.get("file:///home/lilian/liliancai.github.io/RockPaperScissors/index.html")
+        modal=self.browser.find_element_by_css_selector('#ready')
+        modal.click()
+        buttonRock=self.browser.find_element_by_css_selector('#rock')
+
+        for i in range(0,10):
+            buttonRock.click()
+        assert "Game over,start again?" in self.browser.page_source
 
 
 
