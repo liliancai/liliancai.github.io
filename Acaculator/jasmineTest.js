35ed23c6d23b81f211c621accb5af79048da8273
liliancai <yushunchn@gmail.com>
2019-03-18 12:46:50 +0100

    learn jasmin properly!

--- Acaculator/jasmineTest.js
+++ Acaculator/jasmineTest.js
@@ -1,5 +1,20 @@
-describe("Hello World", function() {
-   it("should Return Hello world",function() {
-      expect(reset()).toEqual('Hello World');
-   });
+describe("Calculator", function() {
+    // it('$inputLine should be none at beginning', function(){
+    //     expect($inputLine.html()).toEqual('');
+    // });
+    it("test reset", function(){
+        expect(reset().length).toEqual(0);
+        // I guess test the $var from another html file is not going to work
+        // Take a break here, and go found some Jasmine tutorial later
+    });
+    it("test overFlow", function(){
+        // overFlow();
+        expect(overFlow().html().length).toEqual(14);
+    });
+    it("test abc = abc", function(){
+        expect(abc).toEqual('abc');
+    });
+   // it("should Return Hello world",function() {
+   //    expect(helloword()).toEqual('Hello World');
+   // });
 });
